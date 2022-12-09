9377e960c401aa21488c0681d0173f1c6908f239
Mylana Bulat <79844557+MylanaBulat@users.noreply.github.com>
2022-12-09 10:08:21 +0000

    Update chapter_5.pl

--- chapter_5.pl
+++ chapter_5.pl
@@ -1,4 +1,4 @@
-accLen([H|T], A, L):- Anew is A+1, accLen(T, Anew, L).
+accLen([_|T], A, L):- Anew is A+1, accLen(T, Anew, L).
 accLen([], A, A).
 
-len(List, Length):- accLen(List, 0, Length).
+len(List, Length):- acclen(List, 0, Length).
